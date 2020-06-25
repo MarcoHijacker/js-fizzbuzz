82bdb6ff259bbdb9e3c3c9b67f6adf624cae563f
Marco <marco.longo@hotmail.it>
2020-06-25 16:22:18 +0200

    Esercizio FizzBuzz miglioramento.

---
+++ js/dubbi.js
@@ -0,0 +1,9 @@
+// Dubbi
+
+var database = [];
+const pigreco = "3.14";
+
+database[4] = pigreco; // L'assegnazione prende sempre il valore di destra e lo 'inietta' in quello a sinistra?
+
+var parola1 = "Abate";
+var parola2 = "abate"; 
