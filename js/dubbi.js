a774675ecc4b48317c9601fb7f178d98a36c0f41
Marco <marco.longo@hotmail.it>
2020-06-25 16:47:11 +0200

    Esercizio FizzBuzz definitivo.

--- js/dubbi.js
+++
@@ -1,9 +0,0 @@
-// Dubbi
-
-var database = [];
-const pigreco = "3.14";
-
-database[4] = pigreco; // L'assegnazione prende sempre il valore di destra e lo 'inietta' in quello a sinistra?
-
-var parola1 = "Abate";
-var parola2 = "abate"; 
--- js/script.js
+++ js/script.js
@@ -11,12 +11,13 @@ for (var i = 0; i < 100; i++) {
 // Faccio scorrere un for da 1 a 100: se trovo un numero multiplo di 3, ma non multiplo di 5, inserisco stringa Fizz nel relativo posto.
 // Se trovo un numero multiplo di 5 e non multiplo di 3, inserisco Buzz. Se è sia multiplo di 3 che di 5, inserisco FizzBuzz.
 for (var i = 0; i < 100; i++) {
-  if (!(listaNumeri[i+1]%3) && listaNumeri[i+1]%5) listaNumeri[i+1] = "Fizz";
-  else if (!(listaNumeri[i+1]%5) && listaNumeri[i+1]%3) listaNumeri[i+1] = "Buzz";
-  else if (!(listaNumeri[i+1]%3) && !(listaNumeri[i+1]%5)) listaNumeri[i+1] = "FizzBuzz";
-}
-
-// Stampo nell'html il contenuto degli array dopo le sostituzioni Fizz e Buzz
-for (var i = 0; i < 100; i++) {
-  document.getElementById('listaNumeri').innerHTML += "<li>" + listaNumeri[i+1] + "</li>";
+  if (listaNumeri[i+1]%3 == 0 && listaNumeri[i+1]%5 != 0) {
+    listaNumeri[i+1] = "Fizz";
+  } else if (listaNumeri[i+1]%5 == 0) {
+      listaNumeri[i+1] = "Buzz";
+    } else if (listaNumeri[i+1]%3 == 0 && listaNumeri[i+1]%5 == 0) {
+      listaNumeri[i+1] = "FizzBuzz";
+    }
+    // Stampo nell'html il contenuto degli array dopo le sostituzioni Fizz e Buzz
+    document.getElementById('listaNumeri').innerHTML += "<li>" + listaNumeri[i+1] + "</li>";
 }
