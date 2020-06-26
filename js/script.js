c4a9f6a21c1fe2eabb53f96bed7bf2d50e718319
Marco <marco.longo@hotmail.it>
2020-06-26 16:20:53 +0200

    Esercizio FizzBuzz definitivo (+ commenti).

--- js/script.js
+++ js/script.js
@@ -8,15 +8,16 @@ for (var i = 0; i < 100; i++) {
   listaNumeri[i+1] = (i+1);
 }
 
-// Faccio scorrere un for da 1 a 100: se trovo un numero multiplo di 3, ma non multiplo di 5, inserisco stringa Fizz nel relativo posto.
-// Se trovo un numero multiplo di 5 e non multiplo di 3, inserisco Buzz. Se è sia multiplo di 3 che di 5, inserisco FizzBuzz.
+// Faccio scorrere un for da 1 a 100: se trovo un numero multiplo di 3 e di 5 contemporaneamente, inserisco stringa FizzBuzz nel relativo posto dell'array.
+// Se trovo un numero multiplo di 3 (e non contemporaneamente multiplo di 5, altrimenti mi sarei fermato al primo IF), inserisco Fizz nel relativo posto dell'array.
+// Se trovo un numero multiplo di 5 (e non contemporaneamente multiplo di 3, altrimenti mi sarei fermato al primo IF), inserisco Buzz nel relativo posto dell'array.
 for (var i = 0; i < 100; i++) {
-  if ((i+1)%3 == 0 && (i+1)%5 != 0) {
-    listaNumeri[i+1] = "Fizz";
-  } else if ((i+1)%5 == 0 && (i+1)%3 != 0) {
+  if ((i+1)%3 == 0 && (i+1)%5 == 0) { // Potevo giustamente usare (i+1)%15 == 0 per ovvie ragioni matematiche
+    listaNumeri[i+1] = "FizzBuzz";
+  } else if ((i+1)%3 == 0) {
+      listaNumeri[i+1] = "Fizz";
+    } else if ((i+1)%5 == 0) {
       listaNumeri[i+1] = "Buzz";
-    } else if ((i+1)%3 == 0 && (i+1)%5 == 0) {
-      listaNumeri[i+1] = "FizzBuzz";
     }
   // Stampo nell'html il contenuto degli array dopo le sostituzioni Fizz e Buzz
   document.getElementById('listaNumeri').innerHTML += "<li>" + listaNumeri[i+1] + "</li>";
@@ -25,12 +26,12 @@ for (var i = 0; i < 100; i++) {
 // BONUS - Versione senza array
 
 // for (var i = 1; i < 100; i++) {
-//   if (i%3 == 0 && i%5 != 0) {
-//     console.log("Fizz");
-//   } else if (i%5 == 0 && i%3 != 0) {
-//     console.log("Buzz");
-//   } else if (i%5 == 0 && i%3 == 0) {
+//   if (i%3 == 0 && i%5 == 0) {
 //     console.log("FizzBuzz");
+//   } else if (i%3 == 0) {
+//     console.log("Fizz");
+//   } else if (i%5 == 0) {
+//     console.log("Buzz");
 //   } else {
 //     console.log(i);
 //   }
